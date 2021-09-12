78f1656237325cba7eba52ce8b7a5683626ee6ae
Yumin <dorayuminlee@gmail.com>
2021-09-12 16:56:50 +0900

    muzzle flash 적용

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -14,10 +14,13 @@ public class FireCtrl : MonoBehaviour
     public Transform firePos;
     public AudioClip fireSfx;
 
+    public MeshRenderer muzzleFlash;
+
     // Start is called before the first frame update
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        muzzleFlash = firePos.GetComponentInChildren<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -37,5 +40,22 @@ public class FireCtrl : MonoBehaviour
         Instantiate(bulletPrefab, firePos.position, firePos.rotation);
         // 사운드 발생 (sound, volume scale)
         audio.PlayOneShot(fireSfx, 0.8f);
+
+        // 총구화염 효과
+        ShowMuzzleFlash();
+    }
+
+    void ShowMuzzleFlash()
+    {
+        /*
+            총 4장으로 분리. (0, 0), (0.5, 0), (0, 0.5), (0.5, 0.5)
+            0 ~ 0.5 사이의 난수 생성
+            Random.Range는 (int, int)면 마지막 인자를 포함 x, (float, float)는 포함 o
+            Random.Range(0, 3) ==> 0, 1, 2 
+            Random.Range(0.0f, 3.0f) ==> 0.0f ~ 3.0f
+        */
+
+        Vector2 offset = new Vector2(Random.Range(0, 2) * 0.5f, Random.Range(0, 2) * 0.5f);
+        muzzleFlash.material.mainTextureOffset = offset;
     }
 }
