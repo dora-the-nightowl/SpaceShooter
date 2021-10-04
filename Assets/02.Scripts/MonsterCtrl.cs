689b031713a899815d9c53734549e9b039edc3fd
Yumin <dorayuminlee@gmail.com>
2021-10-04 10:52:13 +0900

    MonsterCtrl script 추가

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -5,8 +5,24 @@ using UnityEngine.AI;
 
 public class MonsterCtrl : MonoBehaviour
 {
+    // 몬스터의 상태 정의
+    public enum State
+    {
+        IDLE,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+    // 몬스터의 상태를 저장할 변수
+    public State state = State.IDLE;
+
+    public float attackDist = 2.0f;  // 공격 사정거리
+    public float traceDist = 10.0f;  // 추적 사정거리
+
     public Transform playerTr;
     private NavMeshAgent agent;
+
+    public bool isDie = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,4 +36,18 @@ public class MonsterCtrl : MonoBehaviour
     {
         agent.SetDestination(playerTr.position);
     }
+
+    // 몬스터의 상태를 체크하는 coroutine
+    IEnumerator CheckMonsterState()
+    {
+        while (!isDie)
+        {
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
 }
+
+/*
+    유한상태머신(Finite State Machine) FSM
+    - NPC AI 구현하기 위한 기법(방식)
+*/
